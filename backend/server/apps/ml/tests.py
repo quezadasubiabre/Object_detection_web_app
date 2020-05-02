690602164eb6decf957e5e99ded8c86b5071eb62
CRISTOBAL QUEZADA <cristobal.quezada@ug.uchile.cl>
2020-05-02 17:35:34 -0400

    agregar ml code to webpage

--- backend/server/apps/ml/tests.py
+++ backend/server/apps/ml/tests.py
@@ -1,3 +1,6 @@
+import inspect
+from apps.ml.registry import MLRegistry
+
 from django.test import TestCase
 
 from apps.ml.object_detector.object_detector import object_detector
@@ -11,3 +14,21 @@ class MLTests(TestCase):
         response = my_alg.inference(input_data)
         self.assertEqual('OK', response['status'])
   
+
+    def test_registry(self):
+        registry = MLRegistry()
+        self.assertEqual(len(registry.endpoints), 0)
+        endpoint_name = "object_detector"
+        algorithm_object = object_detector()
+        algorithm_name = "object detector"
+        algorithm_status = "production"
+        algorithm_version = "0.0.1"
+        algorithm_owner = "CQuezadaSubiabre"
+        algorithm_description = "Object detector tensorflow API"
+        algorithm_code = inspect.getsource(object_detector)
+        # add to registry
+        registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                    algorithm_status, algorithm_version, algorithm_owner,
+                    algorithm_description, algorithm_code)
+        # there should be one endpoint available
+        self.assertEqual(len(registry.endpoints), 1)
--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -14,3 +14,26 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+
+# ML registry
+import inspect
+from apps.ml.registry import MLRegistry
+from apps.ml.object_detector.object_detector import object_detector
+
+try:
+	#registro
+	registry = MLRegistry()
+	# algoritmo machine learning
+	od = object_detector()
+	registry.add_algorithm(endpoint_name="detector_objetos",
+	                            algorithm_object=od,
+	                            algorithm_name="object detector",
+	                            algorithm_status="production",
+	                            algorithm_version="0.0.1",
+	                            owner="CQuezadaSubiabre",
+	                            algorithm_description="Algoritmo deteccion de objetos tensorflow API",
+	                            algorithm_code='Code')
+
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
